9c41ae2b78bce1beac0eb27f62564737912fd247
nomeer <abdallahnomeer173@gmail.com>
2023-10-16 19:44:13 +0300

    First Commit

---
+++ first_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/nomeer/NoMeEr/Robotics Software/Practice/GitHub/wiki.ros.org.tutorials/first_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/nomeer/NoMeEr/Robotics Software/Practice/GitHub/wiki.ros.org.tutorials/first_ws/devel;/opt/ros/noetic'.split(';') if '/home/nomeer/NoMeEr/Robotics Software/Practice/GitHub/wiki.ros.org.tutorials/first_ws/devel;/opt/ros/noetic' != '' else []
